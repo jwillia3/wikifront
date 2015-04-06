5b2da9f39cd47a56f53e2ce628999b983a2b4ff4
jwillia3 <williams.jerry.jr@gmail.com>
2015-04-06 02:05:19 -0500

    sed page mostly working

--- wikifront.js
+++ wikifront.js
@@ -2,6 +2,10 @@ indexUrl = 'http://en.wikipedia.org/w/index.php?action=raw&title=';
 linkBase = 'http://en.wikipedia.org/wiki/';
 
 function renderWiki(name, wiki) {
+    var mode = '';
+    var citation = 0;
+    var i, m;
+    
     function formatWikiLink(all, link, text) {
         return '<a title="' + link + '" onclick=wikiClickHandler() ' +
             'href=' + linkBase + encodeURI(link) + '>' +
@@ -15,57 +19,92 @@ function renderWiki(name, wiki) {
             ' <img src=' + linkBase + 'File:' + (link || '').replace(/ /g, '_') + '>' +
             text + '</aside>';
     }
-    
-    console.clear();
-    console.log(wiki);
-    
-    var mode = '';
-    var i, m;
-    var disambiguation = 'This article is about ' +
-        name + '$1. For other uses see ' +
-        formatWikiLink(null, name, name + ' (disambiguation)');
-    wiki = wiki
-//        .replace(/{{about|(.*?)}}/g, disambiguation)
-//        .replace(/\[\[Image:(.+?)\|thumb|(.+?)\|(.+?)]]/g, formatImage)
-        .replace(/{{{(.|\n)+?}}}/g, '')
-        .replace(/{{(.|\n)+?}}/g, '')
-        .replace(/<!--(.|\n)*?-->/g, '')
-    
-    var lines = wiki.split('\n').map(function(line) {
-        line = line
+    function formatDisambiguation(all, about) {
+        return 'This article is about ' +
+            name + (about? ' ' + about: '') + 
+            '. For other uses see ' +
+            formatWikiLink(null, name + ' (disambiguation)', name + ' (disambiguation)') + '.';
+    }
+    function formatReference(key, value) {
+        return key == 'url'? '<a href=' + value + '>Link</a>':
+            key == 'title'? '<b>' + value + '</b>':
+            key == 'accessdate'? '':
+            key == 'quote'? '<quote class=cite>' + value + '</quote>':
+            value + '<br>';
+    }
+    function renderField(field) {
+        return field
             // Magic Words
             // ISBN, RFC, PMID, Magic Links etc.
             // {{VARIABLE}}
             .replace(/~*/g, '') // user names
-            .replace(/^======([^=]+)======$/, '<h6>$1</h6>')
-            .replace(/^=====([^=]+)=====$/, '<h5>$1</h5>')
-            .replace(/^====([^=]+)====$/, '<h4>$1</h4>')
-            .replace(/^===([^=]+)===$/, '<h3>$1</h3>')
-            .replace(/^==([^=]+)==$/, '<h2>$1</h2>')
-            .replace(/^=([^=]+)=$/, '<h1>$1</h1>')
-            .replace(/\[\[(.+?)\|(.+?)\|(.+?)]]/g, formatWikiLink)
-            .replace(/\[\[(.+?)\|(.+?)]]()/g, formatWikiLink)
+            .replace(/\[\[([^|\]]+?)\|([^|\]]+?)\|([^|\]]+?)]]()/g, formatWikiLink)
+            .replace(/\[\[([^|\]]+?)\|([^|\]]+?)]]()/g, formatWikiLink)
             .replace(/\[\[(.+?)]]()()/g, formatWikiLink)
             .replace(/\[([^ ]+) (.*)]/g, formatExternalLink)
             .replace(/'''(.*?)'''/g, '<b>$1</b>')
             .replace(/''(.*?)''/g, '<i>$1</i>')
+            .replace(/\{\{start date and age\|(.*?)}}/g, '$1')
+            .replace(/{{Citation needed.*?}}/ig, '<sup>[Citation Needed]</sup>')
+            .replace(/{{About\|(.*?)}}/ig, formatDisambiguation)
+            .replace(/{{r\|reader}}/g, '')
+    }
+    function renderLine(line) {
+        return !line? setMode() + '<p>':
+            /^----/.test(line)? setMode() + '<hr>':
+            /{{Infobox/.test(line)? setMode('infobox'):
+                mode=='infobox' && (m = /^ *\| *([^=]+) *= *(.+)$/.exec(line))?
+                    '<tr><td>' + renderField(m[1]||'') +
+                    '</td><td>' + renderField(m[2]||'') + '</td></tr>':
+            /{{cite/.test(line)? setMode('cite'):
+                mode=='cite' && (m = /^ *\| *([^=]+) *= *(.+)$/.exec(line))?
+                    formatReference(renderField(m[1]), renderField(m[2])):
+            /^}}/.test(line)? setMode():
+            /^ /.test(line)? setMode('pre') + renderField(line):
+            /^\;/.test(line)? '<b>' + renderField(line.substring(1)) + '</b>':
+            /^\*/.test(line)? setMode('*') + '<li>' + renderField(line.substring(1)):
+            /^#/.test(line)? setMode('#') + '<li>' + renderField(line.substring(1)):
+            (m = /^======([^=]+)======$/.exec(line))? setMode() + '<h6>' + m[1] + '</h6>':
+            (m = /^=====([^=]+)=====$/.exec(line))? setMode() + '<h5>' + m[1] + '</h5>':
+            (m = /^====([^=]+)====$/.exec(line))? setMode() + '<h4>' + m[1] + '</h4>':
+            (m = /^===([^=]+)===$/.exec(line))? setMode() + '<h3>' + m[1] + '</h3>':
+            (m = /^==([^=]+)==$/.exec(line))? setMode() + '<h2>' + m[1] + '</h2>':
+            (m = /^=([^=]+)=$/.exec(line))? setMode() + '<h1>' + m[1] + '</h1>':
+            setMode() + renderField(line);
+    }
+    function setMode(newMode) {
+        var same = mode == newMode;
+        var prefix =
+            same? '':
+            mode == 'table'? '</table>':
+            mode == 'infobox'? '</table></aside>':
+            mode == 'cite'? '</aside>':
+            mode == 'pre'? '</pre>':
+            mode == '*'? '</ul>':
+            mode == '#'? '</ol>':
+            '';
+        
+        if (newMode == 'cite' && !same)
+            citation++;
             
-            
-        if (!line)
-            return '<p>';
-        if (/^----/.test(line))
-            return '<hr>';
-        if (line[0] == ' ') {
-            mode = 'pre';
-            return (mode? '': '<pre>\n') + line;
-        }
-        if (m = /^([*#]+) (.*)$/.exec(line)) {
-            return '<li>\n' + m[2];
-        }
-        return line;
-    });
-    console.log(lines.join('\n'));
-    return lines.join('\n');
+        mode = newMode || '';
+        
+        return prefix + (
+            same? '':
+            mode == 'table'? '<table>':
+            mode == 'infobox'? '<aside><table>':
+            mode == 'cite'? '<sup>' + citation + '</sup><aside class=ref>' + citation + '. ':
+            mode == 'pre'? '<pre>':
+            mode == '*'? '<ul>':
+            mode == '#'? '<ol>':
+            '');
+    }
+    
+    wiki = wiki
+        .replace(/<!--(.|\n)*?-->/g, '')
+        .replace(/<[/]?syntaxhighlight/g, '<code')
+    
+    return wiki.split('\n').map(renderLine).join('\n');
 }
 
 function openPage(name, target) {
@@ -93,6 +132,6 @@ function handleSearchEnter() {
 
 function main() {
     document.querySelector('#search').onkeydown = handleSearchEnter;
-    openPage('Adobo');
+    openPage('sed');
 }
 document.addEventListener('DOMContentLoaded', main);
